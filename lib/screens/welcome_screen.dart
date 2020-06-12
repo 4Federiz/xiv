0d84e63af6fd710f134fee6171b8b9d53a9bbadd
Hassan Asqiriba <federiz@msn.com>
2020-06-12 14:22:39 -0700

    Gesture Detection

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -33,112 +33,122 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return ResponsiveWidgets.builder(
       child: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                ContainerResponsive(
-                  margin: EdgeInsets.symmetric(horizontal: 15),
-                  padding: EdgeInsets.only(top: 30),
-                  child: TextField(
-                    onChanged: (v) {
-                      textFieldValue = v;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      icon: Hero(
-                        tag: kHeroTagIntroScreen,
-                        child: Icon(
-                          FFFonts.FFXIVMeteo,
-                          size: 60,
+          child: GestureDetector(
+            onTap: (){
+              FocusScopeNode currentFocus = FocusScope.of(context);
+              if(!currentFocus.hasPrimaryFocus) {currentFocus.unfocus();}
+            },
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  ContainerResponsive(
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    padding: EdgeInsets.only(top: 30),
+                    child: TextField(
+                      onChanged: (v) {
+                        textFieldValue = v;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        icon: Hero(
+                          tag: kHeroTagIntroScreen,
+                          child: Icon(
+                            FFFonts.FFXIVMeteo,
+                            size: 60,
+                          ),
                         ),
+                        labelText: 'Character Name',
+                        hintText: '',
                       ),
-                      labelText: 'Character Name',
-                      hintText: '',
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    ContainerResponsive(
-                      margin: EdgeInsets.all(5),
-                      padding: EdgeInsets.only(left: 10),
-                      child: FButton(
-                        text: '  Search',
-                        toDo: () async {
-                          kLoadingIndicator(context);
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ContainerResponsive(
+                        margin: EdgeInsets.all(5),
+                        padding: EdgeInsets.only(left: 10),
+                        child: FButton(
+                          text: '  Search',
+                          toDo: () async {
+                            kLoadingIndicator(context);
 
-                          dynamic response = await widget.json.fetchData(
-                              textFieldValue.trim(), xivModel.getServer);
+                            dynamic response = await widget.json.fetchData(
+                                textFieldValue.trim(), xivModel.getServer);
 
-                          if (response.statusCode == 200) {
-                            widget.jsonBody = response.body;
-                            widget.decoder = jsonDecode(widget.jsonBody);
-
-                            xivModel.setName =
-                                widget.decoder['Results'][0]['Name'];
-                            xivModel.setID = widget.decoder['Results'][0]['ID'];
-
-                            dynamic secondResponse = await widget.json
-                                .fetchAllDataCharacter(xivModel.getID);
-                            if (secondResponse.statusCode == 200) {
-                              widget.jsonBody = secondResponse.body;
+                            if (response.statusCode == 200) {
+                              widget.jsonBody = response.body;
                               widget.decoder = jsonDecode(widget.jsonBody);
 
-                              xivModel.fillVarsCharacter(widget.decoder);
+                              xivModel.setName =
+                                  widget.decoder['Results'][0]['Name'];
+                              xivModel.setID =
+                                  widget.decoder['Results'][0]['ID'];
 
-                              if (xivModel.getFreeCompanyId != null) {
-                                dynamic thirdResponse = await widget.json
-                                    .fetchAllDataFreeCompany(
-                                        xivModel.getFreeCompanyId);
-                                if (thirdResponse.statusCode == 200) {
-                                  widget.jsonBody = thirdResponse.body;
-                                  widget.decoder = jsonDecode(widget.jsonBody);
-                                  xivModel.fillVarsFC(widget.decoder);
+                              dynamic secondResponse = await widget.json
+                                  .fetchAllDataCharacter(xivModel.getID);
+                              if (secondResponse.statusCode == 200) {
+                                widget.jsonBody = secondResponse.body;
+                                widget.decoder = jsonDecode(widget.jsonBody);
+
+                                xivModel.fillVarsCharacter(widget.decoder);
+
+                                if (xivModel.getFreeCompanyId != null) {
+                                  dynamic thirdResponse = await widget.json
+                                      .fetchAllDataFreeCompany(
+                                          xivModel.getFreeCompanyId);
+                                  if (thirdResponse.statusCode == 200) {
+                                    widget.jsonBody = thirdResponse.body;
+                                    widget.decoder =
+                                        jsonDecode(widget.jsonBody);
+                                    xivModel.fillVarsFC(widget.decoder);
+                                  }
+                                } else {
+                                  xivModel.setFreeCompanyName =
+                                      'Not enlisted in a FC';
                                 }
-                              } else {
-                                xivModel.setFreeCompanyName = 'Not enlisted in a FC';
-                              }
 
-                              Navigator.pushNamedAndRemoveUntil(
-                                context,
-                                kRouteCharacterScreen,
-                                (_) => false,
-                              );
+                                Navigator.pushNamedAndRemoveUntil(
+                                  context,
+                                  kRouteCharacterScreen,
+                                  (_) => false,
+                                );
+                              } else {
+                                print(
+                                    'Error parsing data. Server may be wrong');
+                                Navigator.pop(context);
+                              }
                             } else {
-                              print('Error parsing data. Server may be wrong');
+                              print('Error parsing data. Name may be wrong');
                               Navigator.pop(context);
                             }
-                          } else {
-                            print('Error parsing data. Name may be wrong');
-                            Navigator.pop(context);
-                          }
-                        },
+                          },
+                        ),
+                      ),
+                      VerticalDivider(),
+                      ContainerResponsive(
+                        margin: EdgeInsets.all(10),
+                        child: DropDownMenu(),
+                      ),
+                    ],
+                  ),
+                  Flexible(
+                    child: Center(
+                      child: ContainerResponsive(
+                        width: double.maxFinite,
+                        height: 1080,
+                        heightResponsive: true,
+                        margin: EdgeInsets.only(left: 20, right: 20, bottom: 5),
+                        color: Colors.grey,
+                        child: Icon(FFFonts.SymbolQuestion),
                       ),
                     ),
-                    VerticalDivider(),
-                    ContainerResponsive(
-                      margin: EdgeInsets.all(10),
-                      child: DropDownMenu(),
-                    ),
-                  ],
-                ),
-                Flexible(
-                  child: Center(
-                    child: ContainerResponsive(
-                      width: double.maxFinite,
-                      height: 1080,
-                      heightResponsive: true,
-                      margin: EdgeInsets.only(left: 20, right: 20, bottom: 5),
-                      color: Colors.grey,
-                      child: Icon(FFFonts.SymbolQuestion),
-                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
